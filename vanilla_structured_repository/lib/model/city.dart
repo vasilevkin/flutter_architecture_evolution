b929f42981b5e99a6386b36c369b11614efa6229
Sergey Vasilevkin <svasilevkin@gmail.com>
2020-09-23 08:38:43 +0300

    [vanilla_repo] Add Weather to City model.

--- vanilla_structured_repository/lib/model/city.dart
+++ vanilla_structured_repository/lib/model/city.dart
@@ -1,26 +1,43 @@
+import 'package:vanilla_structured_repository/model/weather.dart';
+
 class City {
   final String name;
   final int woeId;
+  final Weather weather;
 
   City({
     this.name,
     this.woeId,
+    this.weather,
   });
 
   factory City.fromJson(Map<String, dynamic> map) {
+    final weathers = map['weathers'];
     return City(
       name: map['title'],
       woeId: map['woeid'],
+      weather: weathers != null
+          ? (weathers as List).map((e) => Weather.fromJson(e)).toList().first
+          : null,
     );
   }
 
   Map<String, dynamic> toJson() => {
         "title": name,
         "woeid": woeId,
+        "weather": weather.toJson(),
       };
+
+  City fromWeather(Weather weather) {
+    return City(
+      name: name,
+      woeId: woeId,
+      weather: weather,
+    );
+  }
 
   @override
   String toString() {
-    return 'City{name: $name, woeId: $woeId}';
+    return 'City{name: $name, woeId: $woeId, weather: $weather}';
   }
 }
