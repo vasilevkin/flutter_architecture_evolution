7d6d3f7b89a6fc4f0541901e4b1d19320d84f085
Sergey Vasilevkin <svasilevkin@gmail.com>
2020-10-06 07:30:50 +0300

    [bloc] Add Edit city screen and bloc.

---
+++ bloc_weather/lib/bloc/edit_city_bloc.dart
@@ -0,0 +1,8 @@
+import 'package:bloc_weather/bloc/bloc.dart';
+
+class EditCityBloc implements Bloc {
+  @override
+  void dispose() {
+    // TODO: implement dispose
+  }
+}
---
+++ bloc_weather/lib/ui/screens/edit_city_screen.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/widgets.dart';
+
+class EdiCityScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
