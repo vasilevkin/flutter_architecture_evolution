a122cca6332b129c0908e26f3c5ab8d47c4b48d3
Sergei Vasilevkin <svasilevkin@gmail.com>
2021-01-06 10:12:24 +0300

    [redux] Add files structure and dependencies for example.

--- redux_weather/lib/app/app.dart
+++ redux_weather/lib/app/app.dart
@@ -3,6 +3,18 @@ import 'package:flutter/material.dart';
 class ReduxWeatherApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          actions: [
+            FlatButton(
+              onPressed: () {},
+              child: Icon(Icons.redeem),
+            ),
+          ],
+        ),
+        body: Container(),
+      ),
+    );
   }
 }
