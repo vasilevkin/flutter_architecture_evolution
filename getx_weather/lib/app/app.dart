43b62d759458b70e42519d5fc08e0de9627b58c0
Sergei Vasilevkin <svasilevkin@gmail.com>
2021-03-29 07:27:25 +0300

    [getx] Remove redux example demo.

--- getx_weather/lib/app/app.dart
+++ getx_weather/lib/app/app.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_redux/flutter_redux.dart';
-import 'package:redux/redux.dart';
 import 'package:getx_weather/app/app_routes.dart';
 import 'package:getx_weather/app/constants.dart';
 import 'package:getx_weather/data/repository/storage_repo.dart';
@@ -8,12 +7,11 @@ import 'package:getx_weather/redux/cities/city_actions.dart';
 import 'package:getx_weather/redux/redux.dart';
 import 'package:getx_weather/redux/store.dart';
 import 'package:getx_weather/redux/suggestions/suggestion_actions.dart';
-import 'package:getx_weather/redux_example/example_screen.dart';
-import 'package:getx_weather/redux_example/redux/example_store.dart';
 import 'package:getx_weather/ui/screens/add_city_screen.dart';
 import 'package:getx_weather/ui/screens/city_detail_screen.dart';
 import 'package:getx_weather/ui/screens/edit_city_screen.dart';
 import 'package:getx_weather/ui/screens/home_screen.dart';
+import 'package:redux/redux.dart';
 
 class GetxWeatherApp extends StatelessWidget {
   final StorageRepository repo;
@@ -82,18 +80,7 @@ class GetxWeatherApp extends StatelessWidget {
     );
   }
 
-  void exampleMain() async {
-    await ExampleRedux.init();
-  }
-
   Widget _makeExampleScreen() {
-    exampleMain();
-
-    return StoreProvider<ExampleAppState>(
-      store: ExampleRedux.store,
-      child: ExampleHomePage(
-        title: Constants.exampleTitle,
-      ),
-    );
+    return Container();
   }
 }
